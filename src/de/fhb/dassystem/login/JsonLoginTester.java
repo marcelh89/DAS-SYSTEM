61657e0b9830febd0bded529b70e5df90eeda925
MaRcMaN <marcelh89@googlemail.com>
2014-06-18 00:15:26 +0200

    add login tester

---
+++ src/de/fhb/dassystem/login/JsonLoginTester.java
@@ -0,0 +1,36 @@
+package de.fhb.dassystem.login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+public class JsonLoginTester extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public JsonLoginTester() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		PrintWriter out = response.getWriter();
+		JSONObject json = new JSONObject();
+		json.put("name", "Sameera Jayasekara");
+		json.put("email", "codesstore@blogspot.com");
+		out.print(json);
+
+	}
+
+}
