25a7644c254da1db9ab0c81c8a8a796f3a9e3d11
MaRcMaN <marcelh89@googlemail.com>
2014-06-17 22:00:12 +0200

    chatmod static user

--- src/de/fhb/dassystem/chat/WsChat.java
+++ src/de/fhb/dassystem/chat/WsChat.java
@@ -1,41 +1,57 @@
 //This sample is how to use websocket of Tomcat7.0.52.
 //web.xml is not required. Because you can use Servlet3.0 Annotation.
 package de.fhb.dassystem.chat;
- 
+
 import java.io.IOException;
 import java.util.ArrayList;
- 
+import java.util.Date;
+
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
- 
+
+import de.fhb.dassystem.db.entity.User;
+
 @ServerEndpoint(value = "/wschat")
-public class WsChat{
-    private static ArrayList<Session> sessionList = new ArrayList<Session>();
-    
-    @OnOpen
-    public void onOpen(Session session){
-        try{
-            sessionList.add(session);
-            //asynchronous communication
-            session.getBasicRemote().sendText("Hello!");
-        }catch(IOException e){}
-    }
-    
-    @OnClose
-    public void onClose(Session session){
-        sessionList.remove(session);
-    }
-    
-    @OnMessage
-    public void onMessage(String msg){
-        try{
-            for(Session session : sessionList){
-                //asynchronous communication
-                session.getBasicRemote().sendText(msg);
-            }
-        }catch(IOException e){}
-    }
+public class WsChat {
+	private static ArrayList<Session> sessionList = new ArrayList<Session>();
+
+	User user = new User("Peter", "Lustig", "123@123.de", "123", new Date(),
+			false);
+
+	@OnOpen
+	public void onOpen(Session session) {
+		try {
+			sessionList.add(session);
+			// asynchronous communication
+			session.getBasicRemote().sendText("Hello!");
+
+			// GET USER FROM SESSION SCOPE
+			// session.getUserProperties().get("user", user);
+			// session.getUserPrincipal());
+
+		} catch (IOException e) {
+		}
+	}
+
+	@OnClose
+	public void onClose(Session session) {
+		sessionList.remove(session);
+	}
+
+	@OnMessage
+	public void onMessage(String msg) {
+		try {
+			for (Session session : sessionList) {
+				// asynchronous communication
+				session.getBasicRemote().sendText(
+						user.getForename() + user.getSurname().charAt(0) + "> "
+								+ msg);
+
+			}
+		} catch (IOException e) {
+		}
+	}
 }
