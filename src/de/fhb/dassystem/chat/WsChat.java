4cdaea8b055191981c5bb8a2a1e279431155f4b3
MaRcMaN <marcelh89@googlemail.com>
2014-06-17 22:56:01 +0200

    update web.xml, update chat example

--- src/de/fhb/dassystem/chat/WsChat.java
+++ src/de/fhb/dassystem/chat/WsChat.java
@@ -3,55 +3,107 @@
 package de.fhb.dassystem.chat;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
+import javax.websocket.CloseReason;
 import javax.websocket.OnClose;
+import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
-import de.fhb.dassystem.db.entity.User;
-
-@ServerEndpoint(value = "/wschat")
+@ServerEndpoint("/chat")
 public class WsChat {
-	private static ArrayList<Session> sessionList = new ArrayList<Session>();
 
-	User user = new User("Peter", "Lustig", "123@123.de", "123", new Date(),
-			false);
+	private static Set<Session> conns = java.util.Collections
+			.synchronizedSet(new HashSet<Session>());
+	private static Map<Session, String> nickNames = new ConcurrentHashMap<Session, String>();
+
+	private Session currentSession;
+
+	public WsChat() {
+		System.out.println("Constructed!");
+	}
 
 	@OnOpen
 	public void onOpen(Session session) {
-		try {
-			sessionList.add(session);
-			// asynchronous communication
-			session.getBasicRemote().sendText("Hello!");
+		System.out.println("WebSocket opened: " + session.getId());
+		conns.add(session);
+		this.currentSession = session;
+	}
 
-			// GET USER FROM SESSION SCOPE
-			// session.getUserProperties().get("user", user);
-			// session.getUserPrincipal());
+	@OnMessage
+	public void onMessage(String message) {
+		System.out.println("Received: " + message);
+		if (!nickNames.containsKey(currentSession)) {
+			// No nickname has been assigned by now
+			// the first message is the nickname
+			// escape the " character first
+			message = message.replace("\"", "\\\"");
 
-		} catch (IOException e) {
+			// broadcast all the nicknames to him
+			for (String nick : nickNames.values())
+				sendMessageToAll("{\"addUser\":\"" + nick + "\"}");
+
+			// Register the nickname with the
+			nickNames.put(currentSession, message);
+
+			// broadcast him to everyone now
+			String messageToSend = "{\"addUser\":\"" + message + "\"}";
+
+			sendMessageToAll(messageToSend);
+
+		} else {
+			// Broadcast the message
+			String messageToSend = "{\"nickname\":\""
+					+ nickNames.get(currentSession) + "\", \"message\":\""
+					+ message.replace("\"", "\\\"") + "\"}";
+
+			sendMessageToAll(messageToSend);
+
 		}
 	}
 
 	@OnClose
-	public void onClose(Session session) {
-		sessionList.remove(session);
+	public void onClose(Session session, CloseReason reason) {
+		String nick = nickNames.get(session);
+		conns.remove(session);
+		nickNames.remove(session);
+		if (nick != null) {
+			removeUser(nick);
+		}
+		System.out.println("Closing a WebSocket due to "
+				+ reason.getReasonPhrase());
 	}
 
-	@OnMessage
-	public void onMessage(String msg) {
-		try {
-			for (Session session : sessionList) {
-				// asynchronous communication
-				session.getBasicRemote().sendText(
-						user.getForename() + user.getSurname().charAt(0) + "> "
-								+ msg);
-
-			}
-		} catch (IOException e) {
+	@OnError
+	public void onError(Session session, Throwable throwable) {
+		String nick = nickNames.get(session);
+		conns.remove(session);
+		nickNames.remove(session);
+		if (nick != null) {
+			removeUser(nick);
 		}
 	}
+
+	private void removeUser(String username) {
+		String messageToSend = "{\"removeUser\":\"" + username + "\"}";
+		sendMessageToAll(messageToSend);
+
+	}
+
+	private void sendMessageToAll(String message) {
+		for (Session sock : conns) {
+			try {
+				sock.getBasicRemote().sendText(message);
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
 }
